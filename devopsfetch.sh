6cc91a7aa17f8735d5c7e2562b347ef2e72f6e3a
rex <109620980+rexbrown21@users.noreply.github.com>
2024-07-24 17:19:38 +0100

    new update to shell script

--- devopsfetch.sh
+++ devopsfetch.sh
@@ -1,70 +1,85 @@
 #!/bin/bash
 
-# Function to list active ports
-list_ports() {
-    if [ -n "$1" ]; then
-        lsof -i :$1
+# Function to display active ports and services
+function display_ports() {
+    if [ -z "$1" ]; then
+        sudo netstat -tuln
     else
-        ss -tuln
+        sudo netstat -tuln | grep ":$1 "
     fi
 }
 
-# Function to list Docker images and containers
-list_docker() {
-    if [ -n "$1" ]; then
-        docker inspect $1
+# Function to display Docker images and containers
+function display_docker() {
+    if [ -z "$1" ]; then
+        echo "REPOSITORY   TAG       IMAGE ID   CREATED   SIZE"
+        sudo docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.CreatedSince}}\t{{.Size}}"
+        echo
+        echo "CONTAINER ID   IMAGE     COMMAND   CREATED   STATUS    PORTS     NAMES"
+        sudo docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"
     else
-        docker ps -a
-        docker images
+        sudo docker inspect "$1"
     fi
 }
 
-# Function to list Nginx domains and ports
-list_nginx() {
-    if [ -n "$1" ]; then
-        cat /etc/nginx/sites-available/$1
+# Function to display Nginx domains and ports
+function display_nginx() {
+    echo "+----------------------------------+--------------------+-----------------------------------------+"
+    echo "|           Server Domain          |       Proxy        |          Configuration File             |"
+    echo "+----------------------------------+--------------------+-----------------------------------------+"
+
+    for file in /etc/nginx/sites-enabled/*; do
+        server_name=$(grep 'server_name' $file | awk '{print $2}' | tr -d ';')
+        proxy_pass=$(grep 'proxy_pass' $file | awk '{print $2}' | tr -d ';')
+        config_file=$(realpath $file)
+        printf "| %-32s | %-18s | %-39s |\n" "$server_name" "$proxy_pass" "$config_file"
+    done
+
+    echo "+----------------------------------+--------------------+-----------------------------------------+"
+}
+
+# Function to display users and last login times
+function display_users() {
+    if [ -z "$1" ]; then
+        who
     else
-        ls /etc/nginx/sites-available
+        last "$1"
     fi
 }
 
-# Parsing command line arguments
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -p|--port)
-            PORT=$2
-            shift
-            ;;
-        -d|--docker)
-            DOCKER=$2
-            shift
-            ;;
-        -n|--nginx)
-            NGINX=$2
-            shift
-            ;;
-        -h|--help)
-            echo "Usage: $0 [-p port] [-d docker] [-n nginx] [-h help]"
-            exit 0
-            ;;
-        *)
-            echo "Unknown parameter passed: $1"
-            echo "Usage: $0 [-p port] [-d docker] [-n nginx] [-h help]"
-            exit 1
-            ;;
+# Function to display activities within a specified time range
+function display_time_range() {
+    last -s "$1"
+}
+
+# Function to display help
+function display_help() {
+    echo "Usage: $0 [option] [argument]"
+    echo
+    echo "Options:"
+    echo "  -p [port_number]      Display active ports and services. If port_number is provided, display details for that port."
+    echo "  -d [container_name]   Display Docker images and containers. If container_name is provided, display details for that container."
+    echo "  -n [domain]           Display Nginx domains and ports. If domain is provided, display details for that domain."
+    echo "  -u [username]         Display users and last login times. If username is provided, display details for that user."
+    echo "  -t [time_range]       Display activities within a specified time range."
+    echo "  -h                    Display this help message."
+}
+
+# Parse command-line arguments
+while getopts "p:d:n:u:t:h" opt; do
+    case "$opt" in
+        p) display_ports "$OPTARG" ;;
+        d) display_docker "$OPTARG" ;;
+        n) display_nginx "$OPTARG" ;;
+        u) display_users "$OPTARG" ;;
+        t) display_time_range "$OPTARG" ;;
+        h) display_help ;;
+        *) display_help ;;
     esac
-    shift
 done
 
-# Execute the appropriate function based on the arguments
-if [ -n "$PORT" ]; then
-    list_ports $PORT
-elif [ -n "$DOCKER" ]; then
-    list_docker $DOCKER
-elif [ -n "$NGINX" ]; then
-    list_nginx $NGINX
-else
-    echo "No valid arguments provided"
-    echo "Usage: $0 [-p port] [-d docker] [-n nginx] [-h help]"
-    exit 1
+# If no arguments are provided, display help
+if [ $# -eq 0 ]; then
+    display_help
 fi
+
